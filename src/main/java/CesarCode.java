9ef0b35e489fa8d9aa8915404f399906ad79c017
Adam Lupinski <azlupinski@gmail.com>
2018-10-14 09:58:22 +0200

    skończony szyfr cesara, rozpoczete znajdowanie wzorca

--- src/main/java/CesarCode.java
+++ src/main/java/CesarCode.java
@@ -1,41 +1,69 @@
-import com.sun.tools.javac.util.ArrayUtils;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
 
 public class CesarCode {
     public static void main(String[] args) {
         encriptingCaesar();
+        decriptingCaesar();
 
     }
 
-    public static String initAlphabetTable() {
-        return "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+    private static char[] initAlphabetTable() {
+        char[] originTable = "abcdefghijklmnopqrstuvwxyz".toUpperCase().toCharArray();
+        return originTable;
     }
 
     private static String userInput() {
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.toString().toUpperCase();
-        scanner.close();
-        return input;
+        String input = scanner.nextLine();
+        String inputOut = input.toUpperCase();
+        //scanner.close();
+        return inputOut;
     }
 
     private static void encriptingCaesar() {
         String input = userInput();
-        String origin = initAlphabetTable();
-        //char[] originTable = initAlphabetTable();
+        String origin = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+        char[] originTable = initAlphabetTable();
         char[] inputTable = input.toCharArray();
         char[] outputTable = new char[inputTable.length];
+        int indeksOfobj;
         int i = 0;
         for (char obj : inputTable) {
-            outputTable[i] = origin.indexOf(obj)
+            if (obj == ' ') {
+                outputTable[i] = ' ';
+                i++;
+            } else {
+                indeksOfobj = origin.indexOf(obj);
+                outputTable[i] = originTable[(indeksOfobj + 3) % 26];
+                i++;
+            }
         }
-
-
         System.out.println(outputTable);
 
     }
 
-
+    private static void decriptingCaesar() {
+        String input = userInput();
+        String origin = "abcdefghijklmnopqrstuvwxyz".toUpperCase();
+        char[] originTable = initAlphabetTable();
+        char[] inputTable = input.toCharArray();
+        char[] outputTable = new char[inputTable.length];
+        int indeksOfobj;
+        int i = 0;
+        for (char obj : inputTable) {
+            if (obj == ' ') {
+                outputTable[i] = ' ';
+                i++;
+            } else {
+                indeksOfobj = origin.indexOf(obj) - 3;
+                if (indeksOfobj < 0) {
+                    indeksOfobj = 26 + indeksOfobj;
+                }
+                outputTable[i] = originTable[(indeksOfobj) % 26];
+                i++;
+            }
+        }
+        System.out.println(outputTable);
+    }
 }
